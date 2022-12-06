b9146876342c415c63cf7ed2be641a00a4022aec
jose.uscanga <jousmo@outlook.com>
2022-12-05 20:40:17 -0600

    feat: configure eslint with standard, config vite test, husky pre commit and push

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    reporters: ['verbose'],
+    coverage: {
+      all: true,
+      reporter: ['text', 'html', 'lcov'],
+      include: ['**/src/**/*.{js,jsx}'],
+      exclude: [
+        '**/src/main.{js,jsx,ts,tsx}',
+        '**/*.types.{ts,tsx}',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/src/vite-env*'
+      ],
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0
+    }
+  }
 })
